adfe21bfef83afbd84bad22579d3b226fea233cf
dimaswsss <mas.dimas2910@gmail.com>
2022-08-08 09:14:31 +0700

    fix(favicon, seo.tsx): edit files

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,19 +3,18 @@ import { useRouter } from 'next/router';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+  title: 'Dimas Wahyu Saputro',
+  siteName: 'Dimas Wahyu Saputro',
+  description: 'a lifelong learner',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://ddiimmaass.my.id',
   type: 'website',
   robots: 'follow, index',
   /**
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  image: '',
 };
 
 type SeoProps = {
@@ -73,7 +72,7 @@ export default function Seo(props: SeoProps) {
           <meta
             name='author'
             property='article:author'
-            content='Theodorus Clarence'
+            content='Dimas Wahyu Saputro'
           />
         </>
       )}
