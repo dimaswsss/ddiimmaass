e01fd4f389d5433c8b0da487c98893e2146b7240
dimaswsss <mas.dimas2910@gmail.com>
2022-08-08 08:14:43 +0700

    fix(index?): edit homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,99 @@
+import clsx from 'clsx';
+import useLoaded from 'hooks/useLoaded';
 import * as React from 'react';
+import { IoNewspaperSharp } from 'react-icons/io5';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
 
+import Accent from '@/components/Accent';
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
+  const isLoaded = useLoaded();
+
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
 
       <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
+        <section
+          className={clsx(
+            'min-h-main -mt-20 mb-20 flex flex-col justify-center',
+            isLoaded && 'fade-in-start'
+          )}
+        >
+          <article className='layout'>
+            <h2 className='text-2xl md:text-4xl 2xl:text-5xl' data-fade='1'>
+              Hi,
+            </h2>
+            <h1
+              className='mt-1 text-3xl md:text-5xl 2xl:text-6xl'
+              data-fade='2'
             >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
-
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
-            </footer>
-          </div>
+              You can call me <Accent>Dimas!</Accent>
+            </h1>
+            <p
+              className={clsx(
+                'mt-4 max-w-4xl text-gray-700 dark:text-gray-200 md:mt-6',
+                'md:text-lg 2xl:text-xl'
+              )}
+              data-fade='3'
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div
+              data-fade='5'
+              className='mt-8 flex flex-wrap gap-4 md:!text-lg'
+            >
+              <ButtonLink href='#intro'>Read the blog</ButtonLink>
+              <ButtonLink href='/about'>Learn more about me</ButtonLink>
+            </div>
+            <div
+              data-fade='6'
+              className='mt-4 flex flex-wrap gap-4 gap-y-2 md:mt-8'
+            >
+              <UnstyledLink
+                href='https://google.com'
+                className={clsx(
+                  'inline-flex items-center gap-1 text-sm font-medium md:text-base',
+                  'text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white',
+                  'focus:outline-none focus-visible:ring focus-visible:ring-primary-300',
+                  'transition-colors'
+                )}
+              >
+                <IoNewspaperSharp className='shrink-0' />
+                <span>Resume</span>
+              </UnstyledLink>
+              <UnstyledLink
+                href='https://linkedin.com/in/dimasws'
+                className={clsx(
+                  'inline-flex items-center gap-1 text-sm font-medium md:text-base',
+                  'text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white',
+                  'focus:outline-none focus-visible:ring focus-visible:ring-primary-300',
+                  'transition-colors'
+                )}
+              >
+                <SiLinkedin className='shrink-0' />
+                <span>LinkedIn</span>
+              </UnstyledLink>
+              <UnstyledLink
+                href='https://github.com/dimaswsss'
+                className={clsx(
+                  'inline-flex items-center gap-1 text-sm font-medium md:text-base',
+                  'text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white',
+                  'focus:outline-none focus-visible:ring focus-visible:ring-primary-300',
+                  'transition-colors'
+                )}
+              >
+                <SiGithub className='shrink-0' />
+                <span>Github</span>
+              </UnstyledLink>
+            </div>
+          </article>
         </section>
       </main>
     </Layout>
